bb904403656140f53513d8684537c43b81ee9202
ponpon <94180803+ponponmookie@users.noreply.github.com>
2024-06-21 11:55:17 +0700

    Create 65106973-2.dart

---
+++ 65106973-2.dart
@@ -0,0 +1,15 @@
+2. Algorithm check Odd or Event
+  void main() {
+  int number = 15;
+  if (number % 2 == 0) {
+    print('$number is even');
+  } else {
+    print('$number is odd');
+  }
+  for (int i = 1; i <= 5; i++) {
+    print(i);
+  }
+}
+
+
+  
